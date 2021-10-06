7c8276221300eafefc3578ded630c322835c34e9
Magda Soballa <magdalena.soballa@merce.com>
2021-10-06 21:33:37 +0200

    [emerkury] adding summary section #91455

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -15,7 +15,7 @@ async function sendContactData(contactDetails) {
   const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message || 'Something went wrong!');
+    throw new Error(data.message || 'Something went wrong!!!!');
   }
 }
 
